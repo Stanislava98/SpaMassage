8f01b6f41a90e80dec18d0a1365bb7b0d2bfada5
Atamanuik Stanislava <johndoe@example.com>
2023-05-25 11:25:44 +0300

    changed massage-cart on screen 1920

--- src/main/massage-cards/MassageListInformation.js
+++ src/main/massage-cards/MassageListInformation.js
@@ -1,11 +1,15 @@
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import { useMediaQuery } from '@mui/material';
 import massages from 'fake-db/massages';
 import MassageCart from './MassageCart';
 
 const MassageListInformation = () => {
+  const isLargeScreen = useMediaQuery('(min-width: 1920px)');
+  const cols = isLargeScreen ? 4 : 3;
+
   return (
-    <ImageList style={{ marginTop: '30px', display: 'flex', flexWrap: 'wrap', gap: '34px' }}>
+    <ImageList cols={cols} gap={34} style={{ marginTop: '30px' }}>
       {massages.map((item, index) => (
         <ImageListItem key={index} className="massage-cart">
           <MassageCart massage={item} />
